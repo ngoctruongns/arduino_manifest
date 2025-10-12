1ee9d90f6f0523e1028ee9b6c2094502308c58ad
TruongVN <vungoctruong8x@gmail.com>
2025-10-13 02:10:48 +0700

    Update to use serial comm library

--- velocity_control/src/main.cpp
+++ velocity_control/src/main.cpp
@@ -1,65 +1,55 @@
+#include "process_data_packet.h"
+#include "velocity_control.h"
 #include <Arduino.h>
 
-#define BAUD_RATE              19200
-#define STX                    0xAA
-#define ETX                    0xDD
-#define CMD_VEL_PACKAGE_SIZE   7
-#define WHEEL_ENC_PACKAGE_SIZE 11
+// Defne platform to
+#define BAUD_RATE 19200
 
-// Velocity command package format:
-// Big endian format
-// [STX][Left RPM High][Left RPM Low][Right RPM High][Right RPM Low][CRC][ETX]
+/*******          Global variable        ************/
 
-// Format packet: STX | L_MSB | L_LSB | R_MSB | R_LSB | CRC | ETX
-// CRC = XOR of L_MSB, L_LSB, R_MSB, R_LSB
+uint8_t rx_buffer[BUFFER_SIZE] = {0};
+uint8_t tx_buffer[BUFFER_SIZE] = {0};
 
-static uint8_t cmd_buffer[CMD_VEL_PACKAGE_SIZE];
 int16_t left_rpm = 0;
 int16_t right_rpm = 0;
-
-// Wheel encoder package format:
-// Big endian format
-// [STX][L B0][L B1][L B2][L B3][R B0][R B1][R B2][R B3][CRC][ETX]
-static uint8_t encoder_buffer[WHEEL_ENC_PACKAGE_SIZE];
 int32_t left_encoder = 0;
 int32_t right_encoder = 0;
 
 // Send encoder data every 100ms
 unsigned long last_send_time = 0;
-const unsigned long send_interval = 100; // milliseconds
+const unsigned long send_interval = 500; // milliseconds
 
-uint8_t calculateCRC(const uint8_t *data, size_t length)
-{
-    uint8_t crc = 0;
-    for (size_t i = 1; i < length - 2; ++i) { // Exclude STX and ETX, CRC
-        crc ^= data[i];
-    }
-    return crc;
-}
+/*******          Function        ************/
 
 void encoderSend(int32_t left_enc, int32_t right_enc)
 {
-    encoder_buffer[0] = STX;
-    encoder_buffer[1] = (left_enc >> 24) & 0xFF;
-    encoder_buffer[2] = (left_enc >> 16) & 0xFF;
-    encoder_buffer[3] = (left_enc >> 8) & 0xFF;
-    encoder_buffer[4] = left_enc & 0xFF;
-    encoder_buffer[5] = (right_enc >> 24) & 0xFF;
-    encoder_buffer[6] = (right_enc >> 16) & 0xFF;
-    encoder_buffer[7] = (right_enc >> 8) & 0xFF;
-    encoder_buffer[8] = right_enc & 0xFF;
-    encoder_buffer[9] = calculateCRC(encoder_buffer, WHEEL_ENC_PACKAGE_SIZE);
-    encoder_buffer[10] = ETX;
+    WheelEncType enc_data;
+    enc_data.type = WHEEL_ENC_COMMAND;
+    enc_data.left_enc = left_enc;
+    enc_data.right_enc = right_enc;
+    uint8_t data_len = sizeof(WheelEncType);
 
-    // Serial.write(encoder_buffer, WHEEL_ENC_PACKAGE_SIZE);
+    // Prepare data package
+    uint8_t tx_len = encoderAllPackage((uint8_t *)&enc_data, data_len, tx_buffer);
+
+    if (tx_len > 0) {
+        // Send data via Serial
+        Serial.write(tx_buffer, tx_len);
+        Serial.println("Encoder send");
+    }
+
+    printf("Sending: L= %ld, R= %ld\n", left_enc, right_enc);
 }
 
-void decoderCmdVel(void)
+void decoderCmdVel(uint8_t *buff, uint8_t len)
 {
-    left_rpm = (int16_t)((cmd_buffer[1] << 8) | cmd_buffer[2]);
-    right_rpm = (int16_t)((cmd_buffer[3] << 8) | cmd_buffer[4]);
+    CmdVelType cmd_vel;
+    memcpy(&cmd_vel, buff, len);
+    left_rpm  = cmd_vel.left_rpm;
+    right_rpm = cmd_vel.right_rpm;
 
     // Print rpm value
+    printf("Received: L= %d, R= %d\n", left_rpm, right_rpm);
     Serial.print("Received: L= ");
     Serial.print(left_rpm);
     Serial.print(", R= ");
@@ -69,8 +59,12 @@ void decoderCmdVel(void)
 void setup()
 {
     Serial.begin(BAUD_RATE);
-    Serial.println("Waiting for data...");
+    Serial.print("cmd_vel: ");
+    Serial.print(sizeof(CmdVelType));
+    Serial.print(", wheel_enc: ");
+    Serial.println(sizeof(WheelEncType));
     // setup PWM, driver motor...
+    delay(1000); // wait for serial monitor
     last_send_time = millis();
 }
 
@@ -85,33 +79,29 @@ void loop()
         right_encoder--; // replace with actual encoder reading
 
         encoderSend(left_encoder, right_encoder);
-        Serial.println(last_send_time);
+        // Serial.println(last_send_time);
     }
 
-    static uint8_t idx = 0;
+    if (Serial.available()) {
+        uint8_t b = (uint8_t) Serial.read();
 
-    while (Serial.available()) {
-        uint8_t b = Serial.read();
-
-        if (idx == 0 && b != STX)
-            continue; // Wait STX
-        cmd_buffer[idx++] = b;
-
-        if (idx == CMD_VEL_PACKAGE_SIZE) { // Received full package
-            idx = 0;
-            Serial.println("Get full packet");
-            if (cmd_buffer[CMD_VEL_PACKAGE_SIZE - 1] != ETX)
-                continue; // Check ETX
-
-                uint8_t crc = calculateCRC(cmd_buffer, CMD_VEL_PACKAGE_SIZE);
-                if (crc == cmd_buffer[CMD_VEL_PACKAGE_SIZE - 2]) {
-                decoderCmdVel();
-                // setMotorRPM(left_rpm, right_rpm);
-            } else {
-                Serial.print("CMD_VEL Fail CRC: ");
-                Serial.print(crc);
-                Serial.print(" != ");
-                Serial.println(cmd_buffer[CMD_VEL_PACKAGE_SIZE - 2]);
+        uint8_t cmd_len = handleRxByteConcurrent(b, rx_buffer);
+        if (cmd_len > 0) {
+            // A complete package is received
+            // Process command based on type
+            uint8_t cmd_type = rx_buffer[0];
+            switch (cmd_type) {
+                case DEBUG_STRING:
+                    // Handle debug string if needed
+                    break;
+                case CMD_VEL_COMMAND:
+                    if (cmd_len == sizeof(CmdVelType)) {
+                        decoderCmdVel(rx_buffer, cmd_len);
+                    }
+                    break;
+                default:
+                    // Unknown command type
+                    break;
             }
         }
     }
